19234d3d999dad4cdfdbbb08826b1a843ddac14d
billchetch <45006134+billchetch@users.noreply.github.com>
2025-01-22 11:59:43 +0700

    first commit

---
+++ GPSService/GPSService.cs
@@ -0,0 +1,81 @@
+using Chetch.ChetchXMPP;
+using Chetch.ChetchXMPP.Exceptions;
+using Chetch.Database;
+using Chetch.GPS;
+using Chetch.Messaging;
+
+namespace Chetch.GPSService;
+
+
+public class GPSService : ChetchXMPPService<GPSService>
+{
+    #region Constants
+    public const String COMMAND_STATUS = "status";
+    public const String COMMAND_POSITION = "position";
+    //public const String COMMAND_SATELLITES = "satellites";
+    #endregion
+
+    #region Fields
+    GPSManager gpsManager = new GPSManager();
+
+    #endregion
+
+    public GPSService(ILogger<GPSService> Logger) : base(Logger)
+    {
+        ChetchDbContext.Config = Config;
+
+        
+    }
+
+    #region Service Lifecycle
+    protected override Task Execute(CancellationToken stoppingToken)
+    {
+
+        gpsManager.StartRecording();
+
+        return base.Execute(stoppingToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        gpsManager.StopRecording();
+
+        return base.StopAsync(cancellationToken);
+    }
+
+    #endregion
+
+    #region Command handling
+
+    protected override void AddCommands()
+    {
+        AddCommand(COMMAND_POSITION, "Returns current position info (will error if GPS device is not receiving)");
+
+        base.AddCommands();
+    }
+
+    protected override bool HandleCommandReceived(ServiceCommand command, List<object> arguments, Message response)
+    {
+        switch (command.Command)
+        {
+            case COMMAND_STATUS:
+
+                return true;
+
+            case COMMAND_POSITION:
+                if(gpsManager.IsReceivingGPSData)
+                {
+                    throw new ChetchXMPPServiceException("No position data available as device is currently not receiving GPS data");
+                }
+                response.AddValue("Position", gpsManager.CurrentPosition);
+                return true;
+
+            /*case COMMAND_SATELLITES:
+                return true;*/
+
+            default:
+                return base.HandleCommandReceived(command, arguments, response);
+        }
+    }
+    #endregion 
+}
---
+++ GPSService/Program.cs
@@ -0,0 +1,9 @@
+namespace Chetch.GPSService;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GPSService.Run(args);
+    }
+}
